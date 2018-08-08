32cbb949721798f9694acc4d069dcefd3a947595
tanguy chennaux <tanguy.chennaux@gmail.com>
2018-08-08 17:26:51 +0100

    front end draft

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,6 +1,7 @@
 class CategoriesController < ApplicationController
   before_action :set_category, only: [:show, :edit, :update, :destroy]
-  
+  before_action :set_products
+
   def index
     @categories = Category.all
   end
@@ -39,6 +40,10 @@ class CategoriesController < ApplicationController
 
   def set_category
     @category = Category.find(params[:id])
+  end
+
+  def set_products
+    @products = Product.all
   end
 
 end
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -14,6 +14,7 @@ class ProductsController < ApplicationController
 
   def new
     @product = Product.new
+    @categories = Category.all
   end
 
   def create
